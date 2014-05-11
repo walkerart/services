6488d0bb8159b24b14a9047bc7c60d765245b747
Jesse Martinez <jesse@martinezdev.com>
2014-05-11 14:27:09 -0400

    cleaning up presentations common schema to remove tenant-specific fields

--- services/presentation/jaxb/src/main/java/org/collectionspace/services/PresentationJAXBSchema.java
+++ services/presentation/jaxb/src/main/java/org/collectionspace/services/PresentationJAXBSchema.java
@@ -8,12 +8,7 @@ public interface PresentationJAXBSchema {
     final static String PRESENTATION_PRESENTER = "presentationPresenter";
     final static String PRESENTATION_TYPE = "presentationType";
     final static String EXHIBITION_TITLE = "exhibitionTitle";
-    final static String OVERRIDE_EXHIBITION_VENUES = "overrideExhibitionVenues";
     final static String PRESENTATION_LOCATION_INTERNAL = "presentationLocationInternal";
     final static String CIRCULATED_VENUES = "circulatedVenues";
     final static String PRESENTATION_NOTE = "presentationNote";
-    final static String UPDATED_VENUE_NAME = "updatedVenueName";
-    final static String UPDATED_VENUE_START_DATE = "updatedVenueStartDate";
-    final static String UPDATED_VENUE_END_DATE = "updatedVenueEndDate";
-    final static String UPDATED_VENUE_ATTENDANCE = "updatedVenueAttendance";
 }
